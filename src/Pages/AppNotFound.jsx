d91e451e4291e51c7039fe8a2cd0ffc069ae5cda
Subroto Chanda Shuvo <subrotochandashuvo@gmail.com>
2025-11-07 21:44:40 +0600

    apps not found added

--- src/Pages/AppNotFound.jsx
+++ src/Pages/AppNotFound.jsx
@@ -3,9 +3,17 @@ import appError from "../assets/App-Error.png";
 
 const AppNotFound = () => {
     return (
-        <div className='p-20'>
+        <div>
+        <div className='p-20 flex justify-center'>
             <img src={appError} alt=""  className=''/>
         </div> 
+        <div className='text-center'>
+        <h1 className='font-semibold text-5xl'>OPPS!! APP NOT FOUND</h1>
+        <p className=' py-4'>The App you are requesting is not found on our system.  please try another apps</p>
+        <a href="/" className="btn bg-linear-to-r from-[#632EE3] to-[#9F62F2] text-white">Go Back!</a>
+
+        </div>
+        </div>
     );
 };
 
